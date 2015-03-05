f2d6739c7219115c676fc3f052d194af4be1c38d
Andre Matecki <andre@matecki.com.br>
2015-03-05 16:09:59 -0300

    VAMOS LA GALAREEE

--- src/com/ibm/cloudoe/samples/HelloResource.java
+++ src/com/ibm/cloudoe/samples/HelloResource.java
@@ -16,8 +16,8 @@ public class HelloResource {
 		// 'VCAP_SERVICES' contains all the credentials of services bound to this application.
 		// String envServices = System.getenv("VCAP_SERVICES");
 		// JSONObject sysEnv = new JSONObject(System.getenv());
-		
-		return "Hi World!";
+
+		return "Hi World! LALALALALA";
 
 	}
 }
