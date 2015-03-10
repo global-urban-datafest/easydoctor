f4dbd1826083ecd533005e93a9a57f773f929c10
andrematecki <andre@matecki.com.br>
2015-03-09 21:38:14 -0300

    Update TesteBanco.java

--- src/br/com/easydoctor/test/TesteBanco.java
+++ src/br/com/easydoctor/test/TesteBanco.java
@@ -5,7 +5,8 @@ import br.com.easydoctor.sqlhelper.SqlHelper;
 public class TesteBanco {
 
 	public static void main(String[] args){
-
+			
+		int i = 0;
 		SqlHelper helper = new SqlHelper();
 		String cmd = "INSERT INTO [MEDICO]([ID],[CPF],[NOME],[AREA_ATUACAO],[NASCIMENTO])VALUES(8,123456,'Teste','Procto','3-3-1990')";
 		helper.executeUpdate(cmd);
